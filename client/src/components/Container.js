80fcd0bacc8b89b7d049617268a6884960e9aeb1
nurhilaltaskn <nurhilaltaskn@gmail.com>
2021-10-11 16:00:41 +0300

    a user connected
    no error

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -1,25 +1,21 @@
 import { useEffect } from 'react';
 import { connectSocket, subscribeToNewMessages } from '../socketApi';
-import Question from './Question';
+/*import Question from './Question';
 import Options from './Options';
-
-import { useVote } from '../contexts/VoteContext';
+*/
 
 function Container() {
-  const { setOptions } = useVote();
+
 
   useEffect(() => {
     connectSocket();
 
-    subscribeToNewMessages((data) => {
-      setOptions(data);
-    });
-  }, [setOptions]);
+
+  }, []);
 
   return (
     <div>
-      <Question />
-      <Options />
+    
     </div>
   );
 }
